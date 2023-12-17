677044cf20e6db0f261aea78832ee3f4364cc5c4
Emanuel <emanuelolvr@outlook.com>
2023-12-17 19:10:43 -0300

    feat: create mapper email dto to email

---
+++ src/main/java/com/emanuel/vet/email/mapper/EmailMapper.java
@@ -0,0 +1,20 @@
+package com.emanuel.vet.email.mapper;
+
+import com.emanuel.vet.email.domains.Email;
+import com.emanuel.vet.email.dtos.EmailDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface EmailMapper {
+
+    EmailMapper INSTANCE = Mappers.getMapper(EmailMapper.class);
+
+    @Mapping(source = "from", target = "from")
+    @Mapping(source = "to", target = "to")
+    @Mapping(source = "subject", target = "subject")
+    @Mapping(source = "text", target = "text")
+
+    Email EmailDTOToEmail(EmailDTO emailDTO);
+}
