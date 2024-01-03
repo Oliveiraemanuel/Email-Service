a2db9a66ad23cecd0fea2438c9a10ea2a5cc8d07
Emanuel <emanuelolvr@outlook.com>
2024-01-03 00:01:39 -0300

    feat: remove lombok because not found

--- src/main/java/com/emanuel/vet/email/domains/Email.java
+++ src/main/java/com/emanuel/vet/email/domains/Email.java
@@ -3,13 +3,11 @@ package com.emanuel.vet.email.domains;
 import com.emanuel.vet.email.dtos.EmailDTO;
 import com.emanuel.vet.email.enums.StatusEmail;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "email")
-@Data
 public class Email {
 
     @Id
@@ -34,5 +32,53 @@ public class Email {
         this.toEmail = data.to();
         this.subject = data.subject();
         this.text = data.text();
+    }
+
+    public String getFromEmail() {
+        return fromEmail;
+    }
+
+    public void setFromEmail(String fromEmail) {
+        this.fromEmail = fromEmail;
+    }
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getSendDateEmail() {
+        return sendDateEmail;
+    }
+
+    public void setSendDateEmail(LocalDateTime sendDateEmail) {
+        this.sendDateEmail = sendDateEmail;
+    }
+
+    public StatusEmail getStatusEmail() {
+        return statusEmail;
+    }
+
+    public void setStatusEmail(StatusEmail statusEmail) {
+        this.statusEmail = statusEmail;
     }
 }
